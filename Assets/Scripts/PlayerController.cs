2238c937529fed0f534d8ceb2da2382d179a4115
coreygaspar <coreyjgaspar@gmail.com>
2025-01-07 08:20:26 -0500

    1/6/2025 Changes
    - Fixed bug where the player could teleport through walls when turning
    
    - Fixed ground detection when the player is grounded or in air
    
    - Added jump animations and jump mechanic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    public float jumpImpulse = 10f;
+    Vector2 moveInput;
+    TouchingDirections touchingDirections;
+    [SerializeField]
+
+    // public float CurrentMoveSpeed 
+    // {
+    //     get
+    //     {
+    //         if(IsRunning && touchingDirections.IsOnWall)
+    //         {
+    //             return runSpeed;
+    //         }
+
+    //         else
+    //         {
+    //             return 0;
+    //         }
+    //     }
+    // }
+
+private bool _isRunning = false;
+
+public bool IsRunning
+{
+    get
+    {
+        return _isRunning;
+    }
+    set
+    {
+        _isRunning = value;
+        animator.SetBool(AnimationStrings.isRunning, value);
+    }
+}
+
+
+// Crouch Concept 
+
+// private bool _isCrouched = false;
+
+// public bool IsCrouched
+// {
+//     get
+//     {
+//         return _isCrouched;
+//     }
+//     set
+//     {
+//         _isCrouched = value;
+//         animator.SetBool("isCrouched", value);
+//     }
+// }
+
+
+private bool _isFacingRight = true;
+
+public bool IsFacingRight
+{
+    get => _isFacingRight;
+    private set
+    {
+        if (_isFacingRight != value)
+        {
+            // Flip the sprite using flipX instead of scaling
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+            // Flip the collider's direction by adjusting the offset
+            CapsuleCollider2D capsuleCollider = GetComponent<CapsuleCollider2D>();
+            Vector2 colliderOffset = capsuleCollider.offset;
+            colliderOffset.x = -colliderOffset.x; // Flip the X offset
+            capsuleCollider.offset = colliderOffset;
+        }
+        _isFacingRight = value;
+    }
+}
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsRunning = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            // Face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // Face the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context){
+        if (context.started){
+            IsRunning = true;
+        }
+        else if(context.canceled){
+            IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context){
+        // TODO Check if alive as well
+        if(context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+    }
+
+// Crouch Concept 
+
+    // public void OnCrouch(InputAction.CallbackContext context){
+    //     if(context.started){
+    //         IsCrouched = true;
+    //     }
+
+    //     else if(context.canceled){
+    //         IsCrouched = false;
+    //     }
+    // }
+}
